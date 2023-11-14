6b30911b299307129e7502cdd69d57adb64aa5b9
Hunor József Nagy <Nhunor06@gmail.com>
2023-11-14 12:21:32 +0100

    lejárt az idő

---
+++ Lift.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace LiftWPF
+{
+    class Lift
+    {
+        string datum;
+        int kartyaSorszam;
+        int induloSzint;
+        int celSzin;
+        public Lift(string inputLine) 
+        {
+            var inputList = inputLine.Split(' ');
+            this.datum = inputList[0];
+            this.kartyaSorszam = int.Parse(inputList[1]);
+            this.induloSzint = int.Parse(inputList[2]);
+            this.celSzin = int.Parse(inputList[3]);
+        }
+    }
+}
